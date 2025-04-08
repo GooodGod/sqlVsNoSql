9a1b50bae07cc94689c44c2712f9dce8bb4048c1
GooodGod <st.38425@gmail.com>
2025-04-08 05:01:13 +0200

    Changes on 08.04.2025 overnight
    Finished homepage(kinda)

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,5 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    var splide = new Splide( '.splide', {
-        type: "loop",
-        perPage: 1,
-        start: 2,
-        autoplay: true,
-        drag: true
-    } );
-var bar = splide.root.querySelector( '.my-carousel-progress-bar' );
+let toTopBtn = document.querySelector(".scroll-to-top-btn");
 
-// Updates the bar width whenever the carousel moves:
-splide.on( 'mounted move', function () {
-  var end  = splide.Components.Controller.getEnd() + 1;
-  var rate = Math.min( ( splide.index + 1 ) / end, 1 );
-  bar.style.width = String( 100 * rate ) + '%';
-} );
-
-splide.mount();
+toTopBtn.addEventListener("click", ()=>{
+    window.scrollTo({top: 0, behavior: "smooth"});
 });
-
