0089633d20b7b8a0813a648c9742fc57a7d117bd
GooodGod <st.38425@gmail.com>
2025-04-15 03:14:13 +0200

    Finished comparison and szbd pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,35 @@
 let toTopBtn = document.querySelector(".scroll-to-top-btn");
+let tabSwitches = document.querySelectorAll(".tab-switch");
+let tabs = document.querySelectorAll(".tab");
 
-toTopBtn.addEventListener("click", ()=>{
+if(tabSwitches){
+  for(let tabSwitch of tabSwitches){
+    tabSwitch.addEventListener("click", (e)=>{
+      activate(e.target);
+    })
+  }
+}
+
+
+
+function activate(clickedSwitch){
+  for(let tabSwitch of tabSwitches){
+    tabSwitch.classList.remove("active");
+  }
+  clickedSwitch.classList.add("active");
+
+  for(let tab of tabs){
+    tab.classList.remove("active");
+
+    if(tab.dataset.tab == clickedSwitch.dataset.tab){
+      tab.classList.add("active");
+    }
+  }
+}
+
+
+if(toTopBtn){
+  toTopBtn.addEventListener("click", ()=>{
     window.scrollTo({top: 0, behavior: "smooth"});
-});
+  });
+}
